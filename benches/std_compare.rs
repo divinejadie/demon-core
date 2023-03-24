0a9c379dc2898f7e6eaf9c4496b48fbc2e6de764
Amelia Joison <joison.me@gmail.com>
2023-03-24 15:25:35 +1100

    Add benchmark to compare Str with std String

---
+++ benches/std_compare.rs
@@ -0,0 +1,39 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn demon_core_str(string: &str) -> demon_core::Str {
+    demon_core::Str::new(string)
+}
+
+fn std_str(string: &str) -> String {
+    String::from(string)
+}
+
+fn demon_core_len(str: demon_core::Str) -> usize {
+    str.len()
+}
+
+fn std_len(str: String) -> usize {
+    str.len()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("demon core Str, create short", |b| {
+        b.iter(|| demon_core_str(black_box("valid utf-8")))
+    });
+    c.bench_function("demon core Str, create long", |b| {
+        b.iter(|| demon_core_str(black_box("this is a long and valid utf-8")))
+    });
+    c.bench_function("std String create", |b| {
+        b.iter(|| std_str(black_box("valid utf-8")))
+    });
+
+    c.bench_function("demon core Str, len short", |b| {
+        b.iter(|| demon_core_len(black_box(demon_core::Str::new("valid utf-8"))))
+    });
+    c.bench_function("std String len", |b| {
+        b.iter(|| std_len(black_box(String::from("valid utf-8"))))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
