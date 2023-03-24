f8eca5802f648ae955a021d2cc4f216b37eed909
Amelia Joison <joison.me@gmail.com>
2023-03-24 15:58:04 +1100

    Correctly set INLINE_SIZE on 32bit targets

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ mod repr;
 mod str;
 mod vec;
 
+#[cfg(target_pointer_width = "64")]
 const INLINE_SIZE: usize = 23;
+#[cfg(target_pointer_width = "32")]
+const INLINE_SIZE: usize = 11;
+
 pub use crate::str::Str;
 pub use crate::vec::Vector;
